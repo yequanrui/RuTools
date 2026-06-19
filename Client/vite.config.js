85217fa4f2792f1798edf90e0f7ba91736b61b9b
yequanrui <yequanrui@qq.com>
2026-06-20 03:21:10 +0800

    update client

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -23,11 +23,10 @@ export default defineConfig(async () => ({
     //
     // prevent vite from obscuring rust errors
     clearScreen: false,
+    // 2. tauri expects a fixed port, fail if that port is not available
     server: {
         port: 1420,
-        // Tauri expects a fixed port, fail if that port is not available
         strictPort: true,
-        // if the host Tauri is expecting is set, use it
         host: host || false,
         hmr: host
             ? {
@@ -40,18 +39,5 @@ export default defineConfig(async () => ({
             // 3. tell vite to ignore watching `src-tauri`
             ignored: ["**/src-tauri/**"],
         },
-    },
-    // Env variables starting with the item of `envPrefix` will be exposed in tauri's source code through `import.meta.env`.
-    envPrefix: ['VITE_', 'TAURI_ENV_*'],
-    build: {
-        // Tauri uses Chromium on Windows and WebKit on macOS and Linux
-        target:
-            process.env.TAURI_ENV_PLATFORM === 'windows'
-                ? 'chrome105'
-                : 'safari13',
-        // don't minify for debug builds
-        minify: !process.env.TAURI_ENV_DEBUG ? 'esbuild' : false,
-        // produce sourcemaps for debug builds
-        sourcemap: !!process.env.TAURI_ENV_DEBUG,
     },
 }));
